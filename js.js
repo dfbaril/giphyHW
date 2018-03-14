52ca011ef90c93d6f75c764246ebb53675e63366
dfbaril <dfbaril@github.com>
2018-03-13 23:40:00 -0500

    need to add CSS formatting and clean up the jQuery, and create API queries for the .text of the buttons that are created, and add ratings to the GIFs

--- js.js
+++ js.js
@@ -1,19 +1,64 @@
+// ~~DONE SO FAR: I have created a for loop that adds buttons to the HTML that read topics[i]
+// ~~NEXT STEPS: 
+//1.I want the buttons to generate an ajax call for images of topics[i]
+//2. I want to use a for loop to have the resultimages append to the HTML with certain attributes
+//3. poops
+
+
 $(document).ready(function() {
-  var APIkey = "vw4loxV4tgBnuMTh4cJ7C9CCpHZLSkeO";
-  var topics = ["cars", "Formula 1", "chess", "cats", "billiards", "computers", "technology"];
+    var url = "http://api.giphy.com/v1/gifs/search?q="
+    var APIkey = "&api_key=vw4loxV4tgBnuMTh4cJ7C9CCpHZLSkeO";
+    var ten = "&limit=10"
+    var topics = ["cars", "F1", "chess", "cats", "billiards", "computers", "technology"];
+    // var queryURL = url + topics[0] + APIkey + ten;
+
+//OMG syntax errors :gun::head:
+    //ok its fixed
+
+//begin for loop that adds buttons with text
+    for(i = 0; i < topics.length; i++){
+        var newButton = $('<button>');
+        var div = $('#imageButtons');
+        // I will need to add an onclick event listener to this that will add topical images for each time it increments the var topics
+        // newButton.attr();
+        newButton.text(topics[i]);
+        newButton.addClass("query-button");
+        $(div).append(newButton);
+
+        //okay above this line works
+    }
+
+//end for loop
+
+// THIS IS UNNECESSARY
+    // $(newButton).on("click", function() {
+    //     var queryURL = url + topics[i] + APIkey + "&limit=1";
+
+    //     $.ajax({
+    //         url: queryURL,
+    //         method: "GET"
+    //     }).then(function(response) {
+    //         console.log(response);
+    //     })
+    // }
+// )})
 
 
-  $(document).on("click", "#cars-button", function() {
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics[0]
-    + "&api_key=" + APIkey + "&limit=5";
 
+
+
+  $(document).on("click", ".query-button", function() {
+    var queryTerm = $(this).text();
+    console.log(queryTerm);
+    var queryURL = url + queryTerm + APIkey + ten;
+    var carsDiv = $('#cars');
+    carsDiv.empty();
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
       console.log(response);
       var giphyResults = response.data;
-      var carsDiv = $('#cars');
       for (var i = 0; i < giphyResults.length; i++) {
         var resultImg = $('<img>');
         var animated = response.data[i].images.original.url;
@@ -34,13 +79,8 @@ $(document).ready(function() {
           }
 
         });
-
         carsDiv.prepend(resultImg);
       }
-
     });
-
-
-
   });
 });
